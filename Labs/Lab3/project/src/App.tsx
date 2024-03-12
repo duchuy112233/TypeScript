0650dcd6f2e0f5a688232b7d0e689c02a102019e
Nguyễn Đức Huy - PH33852 <139211478+duchuy112233@users.noreply.github.com>
2024-03-12 11:02:03 +0700

    cart

--- Labs/Lab3/project/src/App.tsx
+++ Labs/Lab3/project/src/App.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
 
 import './App.css'
-import { productType } from './types/product'
-import { userType } from './types/user'
+import { productType } from './types/Product'
+import { userType } from './types/User'
+import { cartType } from './types/Cart'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -25,6 +26,67 @@ function App() {
       'https://cdn.dummyjson.com/product-images/1/4.jpg',
       'https://cdn.dummyjson.com/product-images/1/thumbnail.jpg'
     ]
+  }
+
+  const cart: cartType = {
+    id: 1,
+    products: [
+      {
+        id: 59,
+        title: 'Spring and summershoes',
+        price: 20,
+        quantity: 3,
+        total: 60,
+        discountPercentage: 8.71,
+        discountedPrice: 55,
+        thumbnail: 'https://cdn.dummyjson.com/product-images/59/thumbnail.jpg'
+      },
+      {
+        id: 88,
+        title: ' TC Reusable Silicone Magic Washing Gloves',
+        price: 29,
+        quantity: 2,
+        total: 58,
+        discountPercentage: 3.19,
+        discountedPrice: 56,
+        thumbnail: 'https://cdn.dummyjson.com/product-images/88/thumbnail.jpg'
+      },
+      {
+        id: 18,
+        title: 'Oil Free Moisturizer 100ml',
+        price: 40,
+        quantity: 2,
+        total: 80,
+        discountPercentage: 13.1,
+        discountedPrice: 70,
+        thumbnail: ' https://cdn.dummyjson.com/product-images/18/thumbnail.jpg'
+      },
+      {
+        id: 95,
+        title: 'Wholesale cargo lashing Belt',
+        price: 930,
+        quantity: 1,
+        total: 930,
+        discountPercentage: 17.67,
+        discountedPrice: 766,
+        thumbnail: 'https://cdn.dummyjson.com/product-images/95/thumbnail.jpg'
+      },
+      {
+        id: 39,
+        title: 'Women Sweaters Wool',
+        price: 600,
+        quantity: 2,
+        total: 1200,
+        discountPercentage: 17.2,
+        discountedPrice: 994,
+        thumbnail: 'https://cdn.dummyjson.com/product-images/39/thumbnail.jpg'
+      }
+    ],
+    total: 2328,
+    discountedTotal: 1941,
+    userId: 97,
+    totalProducts: 5,
+    totalQuantity: 10
   }
 
   const user: userType = {
@@ -96,18 +158,38 @@ function App() {
 
   return (
     <>
-      <div>
-        <h2>{product.title}</h2>
-        <img src={product.thumbnail} alt={product.title} />
-        <h5>Gia san pham: {product.price}</h5>
+      <div className='p-4 border border-gray-300 rounded-md bg-white'>
+        <h1 className='text-xl font-bold text-indigo-600'>{product.title}</h1>
+        <p className='text-gray-700'>{product.description}</p>
+        <p className='text-gray-900'>Price: ${product.price}</p>
+        <p className='text-green-500'>Discount: {product.discountPercentage}%</p>
+        <p className='text-yellow-400'>Rating: {product.rating}</p>
+        <p className='text-blue-500'>Stock: {product.stock}</p>
+        <p className='text-indigo-600'>Brand: {product.brand}</p>
+        <p className='text-purple-700'>Category: {product.category}</p>
+        <img src={product.thumbnail} alt={product.title} className='w-48 h-auto mt-4' />
+        <div className='flex mt-4'>
+          {product.images.map((image, index) => (
+            <img key={index} src={image} alt={product.title} className='w-24 h-auto mr-2' />
+          ))}
+        </div>
       </div>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+      <div className='p-4 border border-gray-300 rounded-md bg-white mt-4'>
+        <h2 className='text-xl font-bold'>User Info</h2>
+        <p className='text-purple-700'>ID: {user.id}</p>
+        <p className='text-purple-700'>
+          Name: {user.firstName} {user.lastName}
         </p>
+        <p className='text-purple-700'>Email: {user.email}</p>
+        <p className='text-purple-700'>Phone: {user.phone}</p>
+        <img src={user.image} alt={`${user.firstName} ${user.lastName}`} className='w-24 h-auto mt-4' />
+        <p className='text-purple-700'>Age: {user.age}</p>
+        <p className='text-purple-700'>Gender: {user.gender}</p>
+        <p className='text-purple-700'>Blood Group: {user.bloodGroup}</p>
+        <p className='text-purple-700'>Height: {user.height} cm</p>
+        <p className='text-purple-700'>Weight: {user.weight} kg</p>
+        {/* Hiển thị các thông tin khác của user nếu cần */}
       </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
     </>
   )
 }
